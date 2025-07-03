4a6d61f7e98c7b175793be50aaa168087138e24c
Pkr <pkrsri71@gmail.com>
2025-07-03 06:22:22 +0530

    Controller and Route For Applications added successfully

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
 
@@ -35,6 +36,7 @@ app.use("/api/v1/company",companyRoute);
 //"http://localhost:8000/api/v1/company/register"
 
 app.use("/api/v1/job",jobRoute);
+app.use("/api/v1/application",applicationRoute);
 
 
 
