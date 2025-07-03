1a06e8ca51df580b788e20bd5ba17dbb05c86a27
Pkr <pkrsri71@gmail.com>
2025-07-04 00:02:24 +0530

    Vite Setup Completed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-500 xl'>Hello World Lets build our JobFinder</h1>
+    </>
+  )
+}
+
+export default App
