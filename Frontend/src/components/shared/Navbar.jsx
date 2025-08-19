738ca94f7e41d77c07079720fd67c4b7e5e4d5d0
Pkr <pkrsri71@gmail.com>
2025-08-19 10:16:14 +0530

    updated

--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -114,10 +114,12 @@ import { Button } from '../ui/button'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { LogOut, User2 } from "lucide-react";
 import { Link, useNavigate } from 'react-router-dom'
+import { useSelector } from "react-redux";
 
 
 const Navbar = () => {
-    const user=false
+    
+    const {user}=useSelector(store=>store.auth);
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -158,7 +160,7 @@ const Navbar = () => {
                                 <div className="flex flex-col text-gray-600">
                                     <div className='flex w-fit items-center gap-2 cursor-pointer'>
                                         <User2 />
-                                        <Button variant="link"> View Profile</Button>
+                                        <Button variant="link"> <Link to="/profile">View Profile</Link></Button>
                                     </div>
                                     <div className='flex w-fit items-center gap-2 cursor-pointer'>
                                         <LogOut />
