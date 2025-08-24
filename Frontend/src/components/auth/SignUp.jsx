6d314e5daa2bfbe0f68e02971d5bebfe907ceb7d
Pkr <pkrsri71@gmail.com>
2025-08-24 16:43:59 +0530

    profilePage completed

--- Frontend/src/components/auth/SignUp.jsx
+++ Frontend/src/components/auth/SignUp.jsx
@@ -9,6 +9,8 @@ import { Link,  useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { USER_API_END_POINT } from '@/utils/constant'
 import { toast } from 'sonner'
+import { setLoading } from '@/redux/authSlice'
+import { Loader2 } from 'lucide-react'
 
 const SignUp = () => {
     const [input, setInput] = useState({
@@ -150,10 +152,10 @@ const SignUp = () => {
                             />
                         </div>
                     </div>
-                    {/* {
+                    {
                         loading ? <Button className="w-full my-4"> <Loader2 className='mr-2 h-4 w-4 animate-spin' /> Please wait </Button> : <Button type="submit" className="w-full my-4">Signup</Button>
-                    } */}
-                    <Button type="submit" className="w-full my-4">Signup</Button>
+                    }
+                   
                     <span className='text-sm'>Already have an account? <Link to="/login" className='text-blue-600'>Login</Link></span>
                 </form>
             </div>
--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -4,11 +4,31 @@ import { Button } from '../ui/button'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { LogOut, User2 } from "lucide-react";
 import { Link, useNavigate } from 'react-router-dom'
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios'
+import { USER_API_END_POINT } from '@/utils/constant'
+import { setUser } from '@/redux/authSlice'
+import { toast } from 'sonner'
 
 
 const Navbar = () => {
     const {user}=useSelector(store=>store.auth);
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+
+     const logoutHandler = async () => {
+        try {
+            const res = await axios.get(`${USER_API_END_POINT}/logout`, { withCredentials: true });
+            if (res.data.success) {
+                dispatch(setUser(null));
+                navigate("/");
+                toast.success(res.data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+        }
+    }
     return (
         <div className='bg-white'>
             <div className='flex items-center justify-between mx-auto max-w-7xl h-16'>
@@ -32,18 +52,18 @@ const Navbar = () => {
                             <Popover>
                         <PopoverTrigger asChild>
                             <Avatar className="cursor-pointer">
-                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn" />
                             </Avatar>
                         </PopoverTrigger>
                         <PopoverContent className="w-80">
                             <div>
                                 <div className="flex gap-2 space-y-2">
                                     <Avatar className="cursor-pointer">
-                                        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                        <AvatarImage src={user?.profile?.profilePhoto} alt="@shadcn" />
                                     </Avatar>
                                     <div>
-                                        <h4 className='font-medium'>Prakhar Application</h4>
-                                        <p className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet.</p>
+                                        <h4 className='font-medium'>{user?.fullname}</h4>
+                                        <p className="text-sm text-muted-foreground">{user?.profile?.bio}</p>
                                     </div>
                                 </div>
                                 <div className="flex flex-col text-gray-600">
@@ -53,7 +73,7 @@ const Navbar = () => {
                                     </div>
                                     <div className='flex w-fit items-center gap-2 cursor-pointer'>
                                         <LogOut />
-                                        <Button variant="link">Logout</Button>
+                                        <Button onClick={logoutHandler} variant="link">Logout</Button>
                                     </div>
                                 </div>
                             </div>
