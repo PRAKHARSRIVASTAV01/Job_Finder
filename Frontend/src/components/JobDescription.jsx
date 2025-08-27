b5a9114944ab1eac3d71a5c8464c1fb7e1176fae
Pkr <pkrsri71@gmail.com>
2025-08-28 02:21:47 +0530

    authentication is persistant

--- Frontend/src/components/JobDescription.jsx
+++ Frontend/src/components/JobDescription.jsx
@@ -1,18 +1,20 @@
-import React,{useEffect} from 'react'
+import React,{useEffect,useState} from 'react'
 import { Badge } from './ui/badge'
 import { Button } from './ui/button'
 import { useParams } from 'react-router-dom';
 
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { JOB_API_END_POINT } from '@/utils/constant';
+import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from '@/utils/constant';
 import { setSingleJob } from '@/redux/jobSlice';
+import { toast } from 'sonner';
 
 const JobDescription = () => {
     const {singleJob}=useSelector(store=>store.job);
     const {user} =useSelector(store=>store.auth);
     const isIntiallyApplied=singleJob?.applications?.some(application => application.applicant === user?._id) || false;
     const [isApplied,setIsApplied]=useState(isIntiallyApplied);
+
     const params = useParams();
     const jobId = params.id;
     const dispatch=useDispatch();
@@ -24,9 +26,8 @@ const JobDescription = () => {
             
             if(res.data.success){
                 setIsApplied(true); // Update the local state
-                //const updatedSingleJob = {...singleJob, applications:[...singleJob.applications,{applicant:user?._id}]}
-                //dispatch(setSingleJob(updatedSingleJob)); // helps us to real time UI update
-                dispatch(setSingleJob(res.data.job));
+                const updatedSingleJob = {...singleJob, applications:[...singleJob.applications,{applicant:user?._id}]}
+                dispatch(setSingleJob(updatedSingleJob)); // helps us to real time UI update
                 toast.success(res.data.message);
 
             }
@@ -42,6 +43,7 @@ const JobDescription = () => {
                 const res = await axios.get(`${JOB_API_END_POINT}/get/${jobId}`,{withCredentials:true});
                 if(res.data.success){
                     dispatch(setSingleJob(res.data.job));
+                    setIsApplied(res.data.job.applications.some(application=>application.applicant === user?._id))
                 }
             } catch (error) {
                 console.log(error);
